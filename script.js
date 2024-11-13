04e846194580f194ee548242181a22ff18c2598c
danikk314 <145961172+danikk314@users.noreply.github.com>
2024-11-13 09:58:58 +0100

    c7

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-let x = "";
+let text = "";
+
+document.getElementById('Indicator').style.backgroundColor = "yellow";
+
+const options = {
+   username: 'Daniel_Braun',  
+   password: '31415926535Pi',
+   clientId: 'mqtt_client_' + Math.random().toString(16).substr(2, 8), 
+   clean: true               
+};
+
+const client = mqtt.connect('wss://xfa25392.ala.eu-central-1.emqxsl.com:8084/mqtt', options);
+
+client.on('connect', () => {
+   console.log('Mit dem MQTT-Broker verbunden');
+   document.getElementById('Indicator').style.backgroundColor = "green";
+});
+
 function sendMessage() {
-   x = document.getElementById('intext').value; 
-   if (x === "Hs") {document.getElementById('intext').value = "";}
+   text = document.getElementById('intext').value; 
+   document.getElementById('intext').value = "";
 }
