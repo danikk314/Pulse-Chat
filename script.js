4352b57f7ca8f5ded6096697f112356b7c6e6288
danikk314 <145961172+danikk314@users.noreply.github.com>
2024-11-14 12:54:07 +0100

    c14

--- script.js
+++ script.js
@@ -17,6 +17,15 @@ client.on('connect', () => {
    document.getElementById('indicator').style.backgroundColor = "green";
 });
 
+   client.subscribe('msg1', (err) => {
+      if (err) {
+         console.error('Abonnement-Fehler:', err);
+      } else {
+         console.log('Erfolgreich abonniert auf Topic: msg1');
+      }
+   });
+});
+
 client.on('error', (error) => {
    console.error('Verbindungsfehler:', error);
    document.getElementById('indicator').style.backgroundColor = "red";
@@ -27,6 +36,12 @@ client.on('disconnect', () => {
    document.getElementById('indicator').style.backgroundColor = "red";
 });
 
+client.on('message', (topic, message) => {
+   if (topic === 'msg1') {  // Prüfen, ob das Topic mit "msg1" übereinstimmt
+      console.log('Nachricht empfangen:', message.toString());
+      document.getElementById('msg1').innerText = message.toString();
+   }
+});
 
 function sendMessage() {
    text = document.getElementById('intext').value;
