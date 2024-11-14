054c0530a7131b03dd6b11e725b8c1570f42bbcc
Danikk314 <da.bra3109@gmail.com>
2024-11-14 15:49:07 +0100

    c16

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let text = "";
+let NewMsg = "";
+let payload = "";
+let ClientName = "";
 
-document.getElementById('indicator').style.backgroundColor = "yellow";
 console.log("Start");
 
 const options = {
@@ -14,7 +16,7 @@ const client = mqtt.connect('wss://xfa25392.ala.eu-central-1.emqxsl.com:8084/mqt
 
 client.on('connect', () => {
    console.log('Mit dem MQTT-Broker verbunden');
-   document.getElementById('indicator').style.backgroundColor = "green";
+   document.getElementById('connection').src = "wifi.png";
 });
 
    client.subscribe('msg1', (err) => {
@@ -27,24 +29,40 @@ client.on('connect', () => {
 
 client.on('error', (error) => {
    console.error('Verbindungsfehler:', error);
-   document.getElementById('indicator').style.backgroundColor = "red";
+   document.getElementById('connection').src = "no-wifi.png";
 });
 
 client.on('disconnect', () => {
    console.log('Vom Broker getrennt');
-   document.getElementById('indicator').style.backgroundColor = "red";
+   document.getElementById('connection').src = "no-wifi.png";
 });
 
 client.on('message', (topic, message) => {
-   if (topic === 'msg1') {  // Prüfen, ob das Topic mit "msg1" übereinstimmt
+   if (topic === 'msg1') { 
       console.log('Nachricht empfangen:', message.toString());
-      document.getElementById('msg1').innerText = message.toString();
+      NewMsg = message.toString();
+      document.getElementById('msg10').innerText = document.getElementById('msg9').innerText;
+      document.getElementById('msg9').innerText = document.getElementById('msg8').innerText;
+      document.getElementById('msg8').innerText = document.getElementById('msg7').innerText;
+      document.getElementById('msg7').innerText = document.getElementById('msg6').innerText;
+      document.getElementById('msg6').innerText = document.getElementById('msg5').innerText;
+      document.getElementById('msg5').innerText = document.getElementById('msg4').innerText;
+      document.getElementById('msg4').innerText = document.getElementById('msg3').innerText;
+      document.getElementById('msg3').innerText = document.getElementById('msg2').innerText;
+      document.getElementById('msg2').innerText = document.getElementById('msg1').innerText;
+      document.getElementById('msg1').innerText = NewMsg;
+
    }
 });
 
 function sendMessage() {
    text = document.getElementById('intext').value;
+   ClientName = document.getElementById('name').value;
+   if (ClientName === "") {ClientName = "Anonym";}
+
    document.getElementById('intext').value = "";
-   client.publish('msg1', text); 
+
+   payload = `${ClientName}: ${text}`;
+   client.publish('msg1', payload); 
    console.log('Nachricht gesendet:', text);
 }
